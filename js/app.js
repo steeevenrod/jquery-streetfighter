e279dbe7ff8fc0d70dc1494e24f6c77073d4b6a1
steeevenrod <steven.rod9@gmail.com>
2016-03-08 14:17:20 -0600

    Added Keycode 88

--- js/app.js
+++ js/app.js
@@ -1,49 +1,49 @@
 $(document).ready(function() {
-	$('.ryu').mouseenter(function() {
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
 
-.mouseleave(function() {
-	$('.ryu-ready').hide();
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
     $('.ryu-still').show();
-})
-.mousedown(function() {
-	playHadouken();
-	$('.ryu-ready').hide();
+  })
+  .mousedown(function() {
+    playHadouken();
+    $('.ryu-still').hide();
+    $('.ryu-ready').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show()
     .animate(
-    {'left': '1020px'},
-    500,
-    function() {
-    $(this).hide();
-    $(this).css('left', '520px');
-    });
-	
-})
+      {'left': '1020px'},
+      500,
+      function() {
+        $(this).stop();
+        $(this).hide();
+        $(this).css('left', '520px');
+      }
+    );
 
-.mouseup(function() {
-	$('.ryu-throwing').hide();
+  })
+
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
     $('.ryu-ready').show();
-});
+  });
+$(document).keydown(function(e){
+    if(e.keyCode == 88) {
+      $('.ryu-still').hide();
+      $('.ryu-ready').hide();
+      $('.ryu-cool').show();
+    }
+  })
+  .keyup(function(e){
+    $('.ryu-cool').hide();
+    $('.ryu-still').show();
+  });
 });
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
-  $(document).keydown(function(e) {
-    if (e.keyCode == 88) {
-      playCool();
-      $('.ryu-action').hide();
-      $('.ryu-cool').show();
-    }
-      }).keyup(function(e) {
-    if (e.keyCode == 88) {
-      $('#cool')[0].pause();
-      $('#cool')[0].load();
-      $('.ryu-cool').hide();
-      $('.ryu-still').show();
-    }
-  });
